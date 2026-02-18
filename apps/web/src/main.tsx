15da583149f21c8d3978dfa94a5b7372841cbed6
gismalink <gismalink@gmail.com>
2026-02-18 10:06:32 +0300

    refactor(web): organize sass with shared partials

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './pages/App';
-import './styles/global.scss';
-import './styles/personnel.scss';
-import './styles/roles.scss';
-import './styles/timeline.scss';
-import './styles/modals.scss';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
