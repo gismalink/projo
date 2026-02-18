50b3ecfbc348f86c17c7a5673554781ba04100f7
gismalink <gismalink@gmail.com>
2026-02-18 10:04:03 +0300

    chore(web): migrate stylesheets to scss

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './pages/App';
-import './styles/global.css';
-import './styles/personnel.css';
-import './styles/roles.css';
-import './styles/timeline.css';
-import './styles/modals.css';
+import './styles/global.scss';
+import './styles/personnel.scss';
+import './styles/roles.scss';
+import './styles/timeline.scss';
+import './styles/modals.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
