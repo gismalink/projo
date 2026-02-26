8a20de1c627283236b847fb0d3aa64522a93aec7
gismalink <gismalink@gmail.com>
2026-02-27 01:49:53 +0300

    fix(import): wire UsersModule into imports module

--- apps/api/src/imports/imports.module.ts
+++ apps/api/src/imports/imports.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
 import { ImportsController } from './imports.controller';
 import { ImportsService } from './imports.service';
 
 @Module({
+  imports: [UsersModule],
   controllers: [ImportsController],
   providers: [ImportsService],
 })
